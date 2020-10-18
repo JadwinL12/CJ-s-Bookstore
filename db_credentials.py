dea12e50f52bc2fa7149696bfbbeead2e3000a22
lichu <lichunlin_78@outlook.com>
2020-10-17 22:41:26 -0400

    Add credential file

---
+++ db_credentials.py
@@ -0,0 +1,8 @@
+
+# For the Local Server
+
+# host = 'classmysql.engr.oregonstate.edu'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
+# user = 'cs340_ONID' #CHANGE ME
+# passwd = 'super_secret_password' #CHANGE ME
+# db = 'cs340_ONID' #CHANGE ME
+
