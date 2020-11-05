aa22352f0b89a0520c4952197aa92e4732208fde
lichu <lichunlin_78@outlook.com>
2020-11-04 21:52:11 -0500

    Connected to local database and correct show the book list on home page

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
+from flask import request
 from db_connector.db_connector import connect_to_database, execute_query
 
 app = Flask(__name__)
-
+customername = "NewCustomer"
 
 @app.route('/')
 @app.route('/home')
@@ -11,11 +12,41 @@ def home():
     query = "SELECT bookID, title, cost, yearPublish, quantityInStock FROM books;"
     result = execute_query(db_connection, query).fetchall()
     # print(result)
-    return render_template("home.html", books=result)
+    return render_template("home.html", books=result, customername=customername)
 
 
-@app.route('/newcustomer')
+@app.route('/newcustomer', methods=['POST', 'GET'])
 def newcustomer():
+    db_connection = connect_to_database()
+    # if request.method == 'GET':
+    #     query = 'SELECT id,
+    if request.method == "POST":
+        fname = request.form['fname']
+        lname = request.form['lname']
+        email = request.form['email']
+        address = request.form['address']
+        city = request.form['city']
+        state = request.form['state']
+        postcode = request.form['postcode']
+        country = request.form['country']
+
+        query = "SELECT email from customers where email=%s"
+        result = execute_query(db_connection,query,[email]).fetchall()
+        print(result[0])
+        print([email])
+        # If the email not exsist yet, then allow t
+        if result[0][0] == email:
+            error = "Email existing, try again!"
+            return render_template("newcustomer.html", error=error)
+        else:
+            query = 'INSERT INTO customers (firstName, lastName, email, address, city, state, postalCode, country) ' \
+                    'VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
+            data = (fname, lname, email, address, city, state, postcode, country)
+            print(data)
+            execute_query(db_connection,query,data)
+    # query = "SELECT email FROM customers"
+    # result = execute_query(db_connection,query).fetchall()
+    # # print(result)
     return render_template("newcustomer.html")
 
 @app.route('/mypayments')
--- db_credentials.py
+++ db_credentials.py
@@ -1,8 +1,16 @@
 
 # For the Local Server
 
-# host = 'classmysql.engr.oregonstate.edu'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
-# user = 'cs340_ONID' #CHANGE ME
-# passwd = 'super_secret_password' #CHANGE ME
-# db = 'cs340_ONID' #CHANGE ME
+host = 'localhost'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
+user = 'root' #CHANGE ME
+passwd = 'Li123456789!' #CHANGE ME
+db = 'bookstore' #CHANGE ME
 
+
+
+# For the School Server
+
+# host = 'classmysql.engr.oregonstate.edu'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
+# user = 'cs340_lichunli' #CHANGE ME
+# passwd = '9820' #CHANGE ME
+# db = 'cs340_lichunli' #CHANGE ME
