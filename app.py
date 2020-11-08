4fc7718db9e5ab3ae2ae37f82e48e6ade61b526d
lichu <lichunlin_78@outlook.com>
2020-11-07 23:19:46 -0500

    customerinfo.html Page functional

--- app.py
+++ app.py
@@ -15,39 +15,61 @@ def home():
     return render_template("home.html", books=result, customername=customername)
 
 
-@app.route('/newcustomer', methods=['POST', 'GET'])
-def newcustomer():
+@app.route('/customerinfo', methods=['POST', 'GET'])
+def customerinfo():
     db_connection = connect_to_database()
-    # if request.method == 'GET':
-    #     query = 'SELECT id,
     if request.method == "POST":
-        fname = request.form['fname']
-        lname = request.form['lname']
-        email = request.form['email']
-        address = request.form['address']
-        city = request.form['city']
-        state = request.form['state']
-        postcode = request.form['postcode']
-        country = request.form['country']
+        # print(request.form) # will see all the request info from the form
+        if 'customeremail' in request.form:
+            customeremail = request.form['customeremail']
+            customer = execute_query(db_connection, "SELECT * FROM customers WHERE email=%s", [customeremail]).fetchall()
+            print(customer)
+            # If return a matching customer, then update the page
+            if customer:
+                return render_template("customerinfo.html", customer=customer)
+            else:
+                error_not_found = "Can't find your Email. Try again or input all your info below to create a new account."
+                return render_template("customerinfo.html", error=error_not_found)
+        # if customer didn't search email
+        if 'email' in request.form:
+            fname = request.form['fname']
+            lname = request.form['lname']
+            email = request.form['email']
+            address = request.form['address']
+            city = request.form['city']
+            state = request.form['state']
+            postcode = request.form['postcode']
+            country = request.form['country']
+            # All these can't be null
+            if fname and lname and email and address and postcode:
+                query = "SELECT email from customers where email=%s"
+                result = execute_query(db_connection, query, [email]).fetchall()
+                update = ""
+                insert = ""
+                if result==():
+                    print("Test")
+                # If the email already exist, then update the info
+                length = len(result)
+                if len(result)==0:
+                    query = 'INSERT INTO customers (firstName, lastName, email, address, city, state, postalCode, country) ' \
+                        'VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
+                    data = (fname, lname, email, address, city, state, postcode, country)
+                    print(data)
+                    execute_query(db_connection,query,data)
+                    insert = "Your Info Created successfully!"
+                    customer = execute_query(db_connection, "SELECT * FROM customers WHERE email=%s",
+                                             [email]).fetchall()
+                    return render_template("customerinfo.html", insert=insert, customer=customer)
+                else:
+                    query = 'UPDATE customers SET lastname=%s, firstname=%s,address=%s,city=%s,state=%s,postalCode=%s,country=%s WHERE email=%s'
+                    data = (lname,fname,address,city,state,postcode,country,email)
+                    execute_query(db_connection,query,data)
+                    update = "Your Info Updated successfully!"
+                    customer = execute_query(db_connection, "SELECT * FROM customers WHERE email=%s",
+                                             [email]).fetchall()
+                    return render_template("customerinfo.html", update=update, customer=customer)
 
-        query = "SELECT email from customers where email=%s"
-        result = execute_query(db_connection,query,[email]).fetchall()
-        print(result[0])
-        print([email])
-        # If the email not exsist yet, then allow t
-        if result[0][0] == email:
-            error = "Email existing, try again!"
-            return render_template("newcustomer.html", error=error)
-        else:
-            query = 'INSERT INTO customers (firstName, lastName, email, address, city, state, postalCode, country) ' \
-                    'VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
-            data = (fname, lname, email, address, city, state, postcode, country)
-            print(data)
-            execute_query(db_connection,query,data)
-    # query = "SELECT email FROM customers"
-    # result = execute_query(db_connection,query).fetchall()
-    # # print(result)
-    return render_template("newcustomer.html")
+    return render_template("customerinfo.html")
 
 @app.route('/mypayments')
 def mypayments():
@@ -70,4 +92,5 @@ def placeorder():
 
 
 if __name__ == '__main__':
+
     app.run(debug=True)
