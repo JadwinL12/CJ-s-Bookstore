ab48551cfa9e9939c6381c34b7f7f7e0eb4149ca
lichu <lichunlin_78@outlook.com>
2020-11-11 18:10:17 -0500

    XX

--- db_credentials.py
+++ db_credentials.py
@@ -8,9 +8,3 @@ db = 'bookstore' #CHANGE ME
 
 
 
-# For the School Server
-
-# host = 'classmysql.engr.oregonstate.edu'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
-# user = 'cs340_lichunli' #CHANGE ME
-# passwd = '9820' #CHANGE ME
-# db = 'cs340_lichunli' #CHANGE ME
